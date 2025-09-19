9209619f733e2afeda52cf50c76fecaaae2f209c
FDH-12MD <24x55a0512@srecnandyal.edu.in>
2025-09-19 11:49:38 +0530

    File Initialisation

---
+++ Basics/Datatypes.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+public class Datatypes {
+    public static void main(String[] args) {
+        int num = 10;
+        double dbl = 10.0;
+        float flt = 10.0f;
+        char chr = 'A';
+        String str = "String";
+        boolean bool = true;
+
+        System.out.println("int: " + num);
+        System.out.println("double: " + dbl);
+        System.out.println("float: " + flt);
+        System.out.println("char: " + chr);
+        System.out.println("String: " + str);
+        System.out.println("boolean: " + bool);
+    }
+}
