b290c408b4dce0dff8523bd2d6e851862414faf7
FDH-12MD <24x55a0512@srecnandyal.edu.in>
2025-09-19 12:50:33 +0530

    File Initialisation

---
+++ Basics/Strings.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Strings{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string");
+        String str = sc.nextLine();
+        System.out.println("The entered string is " + str + "\n The lenght of the string is: "+ str.length());
+        int count = 0;
+        int whitespace = 0;
+        System.out.println("The convertion of string to upper case: "+str.toUpperCase());
+        System.out.println("Count of the String without white space"+ count + whitespace);
+    }
+}
